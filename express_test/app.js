c69165fd87961b7bbf49228bb189f24ced6060c4
Seyoung Hong <maiorem00@gmail.com>
2021-06-16 20:39:14 +0900

    rest-ful

--- express_test/app.js
+++ express_test/app.js
@@ -89,34 +89,11 @@ app.put('/posts', (req,res)=>{
 });
 
 app.delete('/posts/:id', (req,res)=>{
-    if (req.params.id==='1') {
-        res.json({
-            postId: 1,
-            id: 1,
-            name: "id labore ex et quam laborum",
-            email: "Eliseo@gardner.biz",
-            body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-        });
-    } else if (req.params.id==='2') {
-        res.json({
-            postId: 1,
-            id: 2,
-            name: "quo vero reiciendis velit similique earum",
-            email: "Jayne_Kuhic@sydney.com",
-            body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-        });
-    } else if (req.params.id==='3') {
-        res.json({
-            postId: 1,
-            id: 3,
-            name: "odio adipisci rerum aut animi",
-            email: "Nikita@garfield.biz",
-            body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
-        });
-    } else {
-        res.json('error....')
-    }
+    res.json(req.params.id+"번 글 삭제");
 });
+
+
+
 
 app.get('/users', (req, res)=>{
     res.redirect('/users/1');
